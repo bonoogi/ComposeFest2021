0e821683eb9a80912d8ed623a0c77883cde55181
Koo Bonwook <bnoo1333@gmail.com>
2021-11-13 14:51:28 +0900

    이미지아이템 구현

---
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/bonoogi/week2/views/ImageListItem.kt
@@ -0,0 +1,39 @@
+package com.bonoogi.week2.views
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+
+/**
+ * @author 구본욱(bnoo1333@gmail.com)
+ */
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(data = "https://developer.android.com/images/brand/Android_Robot.png"),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text("아이템 $index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Preview
+@Composable
+fun ImageListItemPreview() {
+    ImageListItem(index = 0)
+}
